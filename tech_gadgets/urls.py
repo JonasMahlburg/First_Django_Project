e0559fdf61e1a4c3dfd0e37ed7038c74af1f090a
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-05 10:39:44 +0200

    display gadgets via slugify

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import start_page_view, single_gadged_view
+from .views import start_page_view, single_gadget_view, single_gadget_slug_view
 
 urlpatterns = [
     path('', start_page_view),
-    path('gadget/<int:gadget_id>', single_gadged_view)
+    path('gadget/<int:gadget_id>', single_gadget_view),
+    path('gadget/<slug:gadget_slug>', single_gadget_slug_view),
+
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.utils.text import slugify
 import json
+
 
 
 from .dummy_data import gadgets
@@ -10,5 +12,16 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("Hey das hat doch gut geklappt!")
 
-def single_gadged_view(request, gadget_id):
-    return JsonResponse(gadgets[gadget_id])
+def single_gadget_view(request, gadget_id):
+    return JsonResponse({"result": slugify(gadgets[1]['name'])})
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = {"result": "nothing"}
+
+    for gadget in gadgets:
+        if slugify(gadget["name"]) == gadget_slug:
+            gadget_match = gadget
+            
+    return JsonResponse(gadget_match)
+
+
